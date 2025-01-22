b45a92ddb005da1121f7ffb6578dca314c0bb017
breadbread1984 <breadbread1984@163.com>
2025-01-22 12:01:08 +0800

    update

---
+++ agent.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+from langchain.agents import load_tools, AgentExecutor
+from langchain.agents.format_scratchpad import format_log_to_str
+from langchain.agents.output_parsers import ReActJsonSingleInputOutputParser
+from langchain.tools.render import render_text_description
+from models import 
